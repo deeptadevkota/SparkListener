a43ce51c4e18369cea4dce02d3abc113069c8728
deeptadevkota <deeptadevkota21@gmail.com>
2022-06-03 19:29:38 +0530

    java-option-set

--- src/main/scala/ApplicationListener.scala
+++ src/main/scala/ApplicationListener.scala
@@ -3,26 +3,26 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.scheduler.{SparkListener, SparkListenerJobEnd, SparkListenerJobStart,
   SparkListenerStageCompleted, SparkListenerTaskEnd, SparkListenerStageSubmitted, SparkListenerTaskStart, SparkListenerApplicationEnd}
 
+import scala.collection.JavaConverters._
+
 import EventManager.EventManager
 
+// $SPARK_HOME/bin/spark-submit --class ApplicationListener --driver-java-options -DHTTP_endpoint=xyz --master local SparkListener.jar
 object ApplicationListener {
   def main(args:Array[String]):Unit= {
 
     val sc = new SparkContext()
-
-
     var em: EventManager = null
-
-    if(args.length == 0)
+    val HTTP_endpoint = System.getProperty("HTTP_endpoint")
+    if(HTTP_endpoint ==null)
     {
       em = new EventManager(sc.applicationId, sc.appName)
     }
     else
     {
-      em = new EventManager(sc.applicationId, sc.appName, args(0))
+      em = new EventManager(sc.applicationId, sc.appName, HTTP_endpoint)
     }
     sc.addSparkListener(em)
-
     sc.setLogLevel("ERROR")
 
     val spark: SparkSession = SparkSession.builder()
